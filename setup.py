fd9dceeb5bb098ab0bb1e4767f301428041cbc15
FachrizFaiz <fachriz.zr@outlook.co.id>
2022-05-06 19:25:39 +0700

    Inital commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    version='1.0.0',
+    url='git@github.com:fachrizzz/mlutils-root.git',
+    author='Fachriz',
+    license='unlicensed',
+    packages=['mlutils-root'],
+    zip_safe=False
+)
